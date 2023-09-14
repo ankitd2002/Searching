12be7e5828bc1facf8db62b9fba262eaab03155c
Ankit Dahiya <71880797+ankitd2002@users.noreply.github.com>
2023-09-14 12:09:01 -0700

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,7 +5,7 @@ int binarySearch(int  arr [], int size, int target) {
 
   int start = 0;
   int end = size-1;
-  int mid = (start+end)/2;
+  int mid = (start+end)/2;   // start + (end - start)/ 2   // start / 2  + end / 2
 
   while(start <= end) {
 
